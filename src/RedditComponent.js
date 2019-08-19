aa104b3131aaebc264393dc5dce53033184d6aff
jjeffreys <jjeffreys@xactlycorp.com>
2019-08-19 16:19:58 -0600

    Added Reddit Component w/ api

--- src/RedditComponent.js
+++ src/RedditComponent.js
@@ -3,24 +3,56 @@ import './css/RedditComponent.css';
 
 class RedditComponent extends Component {
 
+  constructor() {
+    super();
+    this.state = {
+      articles: [],
+      url: [],
+    }
+  }
+
+  componentDidMount() {
+
+    fetch('https://www.reddit.com/r/WorldNews/top/.json?count=5')
+    .then(results => {
+      return results.json();
+    }).then(data => {
+      console.log("data", data.data.children);
+      let articles = data.data.children.map((item) => {
+        console.log("item", item.data.title);
+        return(
+          <div key={item.data.children}>
+            <a href={item.data.url}>
+              [{item.data.score}]
+              -&nbsp;
+              {item.data.title}
+            </a>
+          </div>
+        )
+      })
+      this.setState({articles: articles});
+      console.log("state", this.state.articles);
+    })
+  }
+
   render() {
 
     return(
       <div className="Reddit">
         <h3>
-          Reddit News
+          Today's Reddit News
         </h3>
 
         <div className="RedditElement">
-              Temp Link 1
+          {this.state.articles[0]}
         </div>
 
         <div className="RedditElement">
-              "Temp Link 2"
+          {this.state.articles[1]}
         </div>
 
         <div className="RedditElement">
-              "Temp Link 3"
+          {this.state.articles[2]}
         </div>
 
       </div>
